c80b0e8e20f713ef17a43d3080cbd8487b35954e
WangYU <qiaofei9@foxmail.com>
2015-12-07 15:55:06 +0800

    test swift

---
+++ TestPractise/TestPractise/main.swift
@@ -0,0 +1,28 @@
+//
+//  main.swift
+//  TestPractise
+//
+//  Created by Fei on 15/12/3.
+//  Copyright © 2015年 WangQiaoFei. All rights reserved.
+//
+
+import Foundation
+
+let a = 90
+var b = 120
+let c: Float = 70
+let str = "seventy is "
+let str2 = str + String(c)
+
+//print (a + b)
+//print (str2)
+//print("i have \(b) apples")
+let test = [1,2,3,4,5,6,7,8,9]
+for num in test{
+//    print(num > 5 ? "yes" : "no")
+    if num > 5 {
+    print ("\(num) > 5")}
+    else {
+    print ("\(num) <= 5")}
+}
+

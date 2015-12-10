eb6446b065ad2a473eba111c062dae6271b0f195
Fei <qiaofei9@foxmail.com>
2015-12-10 12:04:58 +0800

    下拉反弹的scrollView

---
+++ TestBounceScrollView/app/src/main/java/com/asia00/testbouncescrollview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.asia00.testbouncescrollview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+}

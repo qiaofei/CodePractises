c3851a2cc9db8e74d902db90378411954528b7f9
WangYU <qiaofei9@foxmail.com>
2015-12-17 20:35:19 +0800

    平铺导航

---
+++ PageControlNavigation/PageControlNavigation/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  PageControlNavigation
+//
+//  Created by Fei on 15/12/16.
+//  Copyright © 2015年 WangQiaoFei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBAction func changePage(sender: AnyObject) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

bae9dc85d417826e31eff6e205c25af3d59243c9
WangYU <qiaofei9@foxmail.com>
2015-12-21 22:56:59 +0800

    ?和!的区别

---
+++ TestQuestionAnd/TestQuestionAnd/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  TestQuestionAnd
+//
+//  Created by Fei on 15/12/21.
+//  Copyright © 2015年 WangQiaoFei. All rights reserved.
+//
+
+import Foundation
+
+//print("Hello, World!")
+//感叹号和问号的用法,因为swift不是给变量赋默认的初始值
+//二者都可以标识变量为可选变量或可空变量
+//!可取可选变量的非空值,?则不可
+
+var a:Int?
+if (a != nil) {
+    print("1");
+} else {
+    print("2");
+}
+
+
+

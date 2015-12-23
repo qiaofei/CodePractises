a6d37b3e135d559ee67c861b6c0382c2c74a08be
WangYU <qiaofei9@foxmail.com>
2015-12-23 23:33:29 +0800

    test bool

--- TestOC/TestOC/main.m
+++ TestOC/TestOC/main.m
@@ -10,9 +10,16 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        BOOL isTrrue = false;
         int i = 0 ;
         // insert code here...
-        NSLog(@"Hello, World!"  i);
+        if (isTrrue) {
+            NSLog(@"is ture");
+        }
+        else{
+            NSLog(@"is false");
+        }
+        NSLog(@"Hello, World!");
 //        print("hello word");
     }
     return 0;

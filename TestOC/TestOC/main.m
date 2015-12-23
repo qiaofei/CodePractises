caacda8fdc83cc51c0a23f73adb39df2d3f9b0ea
WangYU <qiaofei9@foxmail.com>
2015-12-23 23:28:53 +0800

    TestObjectC

---
+++ TestOC/TestOC/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  TestOC
+//
+//  Created by Fei on 15/12/23.
+//  Copyright © 2015年 WangQiaoFei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        int i = 0 ;
+        // insert code here...
+        NSLog(@"Hello, World!"  i);
+//        print("hello word");
+    }
+    return 0;
+}

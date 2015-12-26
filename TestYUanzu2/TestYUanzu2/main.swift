102474380cad4f5cde596e2bc611bf8faa7736d2
WangYU <qiaofei9@foxmail.com>
2015-12-26 23:52:15 +0800

    test元组

---
+++ TestYUanzu2/TestYUanzu2/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  TestYUanzu2
+//
+//  Created by Fei on 15/12/26.
+//  Copyright © 2015年 WangQiaoFei. All rights reserved.
+//
+
+import Foundation
+
+//print("Hello, World!")
+let(x,y) = (1,2)
+print((x,y))
+

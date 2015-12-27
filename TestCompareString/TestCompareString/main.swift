9663e1b6224e5e1b585f46a9d759c3e6f9c553ce
WangYU <qiaofei9@foxmail.com>
2015-12-27 23:13:31 +0800

    字符串相关方法

---
+++ TestCompareString/TestCompareString/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  TestCompareString
+//
+//  Created by Fei on 15/12/27.
+//  Copyright © 2015年 WangQiaoFei. All rights reserved.
+//
+
+import Foundation
+//判断字符串是否相同用==
+let s1 = "qu si ba"
+let s2 = "qu si ba"
+print((s1 == s2 ? "equals":"not equals"))
+
+//通过调用字符串的 hasPrefix/hasSuffix 方法来检查字符串是否拥有特定前缀/后缀
+//uppercaseString 和 lowercaseString 属性来访问一个字符串的大写
+//小写版本。
+
+

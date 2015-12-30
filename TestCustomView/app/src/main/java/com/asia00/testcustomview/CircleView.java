702320b9d33c838eefee63fb6f950da8f7330fe9
Fei <qiaofei9@foxmail.com>
2015-12-30 21:03:06 +0800

    自定义属性

--- TestCustomView/app/src/main/java/com/asia00/testcustomview/CircleView.java
+++ TestCustomView/app/src/main/java/com/asia00/testcustomview/CircleView.java
@@ -1,6 +1,7 @@
 package com.asia00.testcustomview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -25,6 +26,11 @@ public class CircleView extends View {
 
     public CircleView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        //
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CircleView);
+        mColor = a.getColor(R.styleable.CircleView_circle_color, Color.RED);
+        a.recycle();
+        init();
     }
 
     public void init() {
@@ -42,7 +48,23 @@ public class CircleView extends View {
         int height = getHeight() - paddingBottom - paddingTop;
         int radius = Math.min(width, height) / 2;
         //添加padding属性
+        canvas.drawCircle(paddingLeft + width / 2, paddingTop + height / 2, radius, mPaint);
+    }
 
-        canvas.drawCircle(width / 2, height / 2, radius, mPaint);
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //拿到长度和宽度的配置文件
+        int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
+        if (widthSpecMode == MeasureSpec.AT_MOST && heightMeasureSpec == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(200, 200);
+        } else if (widthSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(200, heightSpecSize);
+        } else if (heightSpecMode == MeasureSpec.AT_MOST) {
+            setMeasuredDimension(widthSpecSize, 200);
+        }
     }
 }

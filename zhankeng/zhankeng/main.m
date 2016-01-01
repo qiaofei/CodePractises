3d167f402efae29514d85cb5fbf8b424c56aa134
WangYU <qiaofei9@foxmail.com>
2016-01-01 23:17:19 +0800

    thanking

---
+++ zhankeng/zhankeng/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  zhankeng
+//
+//  Created by Fei on 16/1/1.
+//  Copyright © 2016年 WangQiaoFei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

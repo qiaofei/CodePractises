2effd06e6a38c420fd834c98a41c627af1c5d528
Fei <qiaofei9@foxmail.com>
2016-01-08 21:01:02 +0800

    添加action和Service代码

--- TestSSH/src/action/UserInfoAction.java
+++ TestSSH/src/action/UserInfoAction.java
@@ -3,7 +3,12 @@ package action;
 import biz.UserInfoService;
 
 public class UserInfoAction {
-private UserInfoService userInfoService;
+	private UserInfoService userInfoService;
+
+	// 拿到用户名和用户密码
+	private String userName;
+	private String userPassword;
+
 	public void getUserInfo() {
 	}
 }
--- TestSSH/src/biz/UserInfoService.java
+++ TestSSH/src/biz/UserInfoService.java
@@ -1,8 +1,21 @@
 package biz;
 
+import java.util.List;
 import dao.UserInfoDao;
+import entity.UserInfo;
 
 public class UserInfoService {
 	private UserInfoDao userInfoDao;
 
+	public UserInfo checkLogin(String userName, String userPassword) {
+		String sSql = "from DriverInfo where " + "contactTel = '" + userName
+				+ "' " + "and password = '" + userPassword + "'";
+		// LogUtil.logInfo(UserInfoService.class, sSql);
+		List<UserInfo> list = userInfoDao.getUserInfoBySql(sSql);
+		if (list == null || list.size() <= 0) {
+			return null;
+		} else {
+			return list.get(0);
+		}
+	}
 }

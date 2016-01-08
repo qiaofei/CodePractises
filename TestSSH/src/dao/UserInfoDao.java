3b534d5e19f802a63c447fd4d9734a1381840ef1
Fei <qiaofei9@foxmail.com>
2016-01-08 21:01:18 +0800

    添加dao代码

--- TestSSH/src/dao/UserInfoDao.java
+++ TestSSH/src/dao/UserInfoDao.java
@@ -1,5 +1,22 @@
 package dao;
 
-public class UserInfoDao {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import entity.UserInfo;
+
+public class UserInfoDao {
+	public List<UserInfo> getUserInfoBySql(String sSql) {
+//		 Session session = this.getSession();
+		List<UserInfo> list = new ArrayList<UserInfo>();
+		try {
+			// Query query = session.createQuery(sSql);
+			// list = query.list();
+		} catch (Exception e) {
+		}
+		return list;
+	}
 }

2efef80ce859f03da0afdf16b60ef44b1cbd23e5
WangYU <qiaofei9@foxmail.com>
2016-01-21 11:04:58 +0800

    modelViewTest

---
+++ DelegateDemo/DelegateDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DelegateDemo
+//
+//  Created by Fei on 16/1/20.
+//  Copyright © 2016年 WangQiaoFei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
